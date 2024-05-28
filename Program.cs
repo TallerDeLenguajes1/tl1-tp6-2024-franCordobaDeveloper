62bd517ec0963bbbdddc1827819f0946725704db
franCordobaDeveloper <franCordobaDeveloper>
2024-05-28 15:53:25 -0300

    CalculadoraV1 finalizado

--- Program.cs
+++ Program.cs
@@ -80,3 +80,86 @@ pida dos números y que devuelva el resultado de la operación seleccionada. Ade
 una vez que termine de realizar la operación le pregunte si desea realizar otro cálculo.
 */
 
+// variables
+int opc; 
+int num1, num2, resultado;
+string? numString;
+bool continuar = true;
+
+while(continuar)
+{
+    // Menu
+    do
+    {
+        Console.WriteLine("\nElija una opción : \n");
+        Console.WriteLine("1) Sumar");
+        Console.WriteLine("2) Restar");
+        Console.WriteLine("3) Multiplicar");
+        Console.WriteLine("4) Dividir");
+        Console.WriteLine("0) Salir");
+
+        numString = Console.ReadLine();
+    } while(!int.TryParse(numString, out opc) || opc < 0 || opc > 4);
+
+    if(opc == 0) 
+    {
+        Console.WriteLine("\nElegiste la opción 0 para salir");
+        continuar = false;
+    } 
+    else 
+    {
+        bool control1 = true, control2 = true;
+
+        Console.WriteLine("Ingrese el primer número: ");
+        numString = Console.ReadLine();
+        control1 = int.TryParse(numString, out num1);
+
+        Console.WriteLine("Ingrese el segundo número: ");
+        numString = Console.ReadLine();
+        control2 = int.TryParse(numString, out num2);
+
+        if(control1 && control2)
+        {
+            switch(opc)
+            {
+                case 1:
+                    Console.WriteLine("1) Sumar dos números");
+                    resultado = num1 + num2;
+                    Console.WriteLine("Resultado: " + resultado);
+                    break;
+
+                case 2:
+                    Console.WriteLine("2) Restar dos números");
+                    resultado = num1 - num2;
+                    Console.WriteLine("Resultado: " + resultado);
+                    break;
+
+                case 3:
+                    Console.WriteLine("3) Multiplicar dos números");
+                    resultado = num1 * num2;
+                    Console.WriteLine("Resultado: " + resultado);
+                    break;
+
+                case 4:
+                    Console.WriteLine("4) Dividir dos números");
+                    if(num2 == 0) 
+                    {
+                        Console.WriteLine("No se puede dividir entre 0");
+                    } 
+                    else 
+                    {
+                        resultado = num1 / num2;
+                        Console.WriteLine("Resultado: " + resultado);
+                    }
+                    break;
+            }
+        }
+        Console.WriteLine("\n¿Desea continuar utilizando la calculadora? (s/n): ");
+        string? respuesta = Console.ReadLine();
+        if (respuesta != null && (respuesta != "s" || respuesta != "S"))
+        {
+            continuar = false;
+            Console.WriteLine("Saliendo de calculadora");
+        }
+    }
+}
